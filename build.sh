e57405306ff8cd269aa04ceb6d63d7200548dbea
ItsEntDev <hallamjsclark@gmail.com>
2023-09-23 13:45:14 +0100

    Fix build.sh and update .gitignore

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-if [ "$1" == "Release" ]; then
-	BINARY_DIR="./cmake-build-release"
-elif [ "$1" == "Debug" ]; then
-	BINARY_DIR="./cmake-build-debug"
-else
-	BINARY_DIR="."
+if [ "$2" == "Ninja" ]; then
+	GEN="ninja"
+elif [ "$2" == "Make" ]; then
+	GEN="make"
 fi
-
-echo Building for $1
-cmake -DCMAKE_BUILD_TYPE=$1 -S . -B . -G $2
-mv "$BINARY_DIR/win95-keygen" "./win95-keygen"
+echo Building for "$1"
+cmake -DCMAKE_BUILD_TYPE="$1" -S . -B . -G "$2"
+$GEN
+mv ./linux ./win95-keygen
+echo Build finished.
