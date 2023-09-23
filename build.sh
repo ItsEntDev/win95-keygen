9ee0f9e2d6e2b54fdcc2a5b9ff01582770c12acc
ItsEntDev <hallamjsclark@gmail.com>
2023-09-23 13:50:35 +0100

    Change build.sh to REQUIRE ninja and cmake.

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-if [ "$2" == "Ninja" ]; then
-	GEN="ninja"
-elif [ "$2" == "Make" ]; then
-	GEN="make"
+if ! command -v ninja >/dev/null 2>&1; then
+    echo "Missing ninja."
+    exit
+fi
+if ! command -v cmake >/dev/null 2>&1; then
+    echo "Missing cmake."
+    exit
 fi
 echo Building for "$1"
-cmake -DCMAKE_BUILD_TYPE="$1" -S . -B . -G "$2"
+cmake -DCMAKE_BUILD_TYPE="$1" -S . -B . -G Ninja
 $GEN
 mv ./linux ./win95-keygen
 echo Build finished.
